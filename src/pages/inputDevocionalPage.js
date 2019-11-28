4a93808358cd437b880b43cfb4c285957e289186
Fernanda Stella <fstella@uolinc.com>
2019-11-28 17:25:30 -0300

    Desenvolvendo pages adm

--- src/pages/inputDevocionalPage.js
+++ src/pages/inputDevocionalPage.js
@@ -3,114 +3,146 @@ import {
     StyleSheet,
     Text,
     View,
-    StatusBar,
-    Image,
     TextInput,
     TouchableOpacity,
-    TouchableHighlight,
-    ImageBackground,
-} from "react-native"
+} from "react-native";
+import { Actions } from "react-native-router-flux";
 
 
-export default class InputEventPage extends Component {
+export default class alterPassPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            titlePray: "",
+            authorPray: "",
+            textPray: "",      
+        }
+    }
 
     render() {
         console.log(this.state)
         return (
-            <ImageBackground source={require("../images/tela.jpg")} style={styles.container}>
-                <StatusBar
-                    backgroundColor={"transparent"}
-                    translucent
-                    barStyle={"light-content"}
-                />
-                <View style={styles.containerlogo}>
-                    <Image style={{ width: 150, height: 121 }}
-                        source={require('../images/logo.png')} />
-                    <Text style={styles.logoText}>Estamos na tela input devocional</Text>
-                </View>
-                <View style={styles.singupTextCont}>
-                    <Text style={styles.singupText}>Don't' have an account yet?</Text>
-                    <Text style={styles.signupButton}>Singup</Text>
-                </View>
-            </ImageBackground>
+          <View style={styles.container}>
+
+            <View style={styles.header}>
+               <Image style={styles.containerlogo }
+              source={require('../images/voltar.png')} />
+              <Text style={styles.textHeader}> Novo devocional</Text>
+            </View>
+
+            <View style={styles.container2}>
+
+                    <Image style={styles.imageDevocional}
+                        source={require('../images/banner3.jpg')} /> {/* alterar para imagem de input */}
+
+                    <View style={styles.space}></View>
+                    <View style={styles.space}></View>
+
+                    <TextInput
+                        onChangeText={value => this.setState({ titlePray: value })}
+                        style={styles.inputBox}
+                        placeholder="Título"
+                        underlineColorAndroid="rgba(0,0,0,0)"
+                        placeholderTextColor="#ffffff"
+                    />
+                    <TextInput
+                        onChangeText={value => this.setState({ authorPray: value })}
+                        style={styles.inputBox}
+                        placeholder="Nome do autor"
+                        underlineColorAndroid="rgba(0,0,0,0)"
+                        placeholderTextColor="#ffffff"
+                    />
+                    <TextInput
+                        onChangeText={value => this.setState({ textPray: value })}
+                        style={styles.inputBox2}
+                        placeholder="Digite o devocional aqui..."
+                        underlineColorAndroid="rgba(0,0,0,0)"
+                        placeholderTextColor="#ffffff"
+                    />
+                    
+                    <TouchableOpacity onPress={() => Actions.alert()} style={styles.button}>
+                        <Text style={styles.registerButton}>Enviar</Text>
+                    </TouchableOpacity>
+              </View>
+            </View>
         )
     }
-
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#455a64",
+        backgroundColor: "#3E3E3E",
+    },
+    container2: {
+      flex: 1,
+      backgroundColor: "#3E3E3E",
+      alignItems: "center",
+  },
+    registerButton: {
+        color: "#ffffff",
+        fontSize: 16,
+        fontWeight: '500',  
         justifyContent: "center",
         alignItems: "center",
     },
-    viewLogo: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center"
-    },
-    singupText: {
-        color: "rgba(255,255,255,0.6)",
-        fontSize: 16
-    },
-    singupTextCont: {
-        flexGrow: 1,
-        justifyContent: "center",
-        alignItems: "flex-end",
-        paddingVertical: 16,
-        flexDirection: "row"
-    },
-    viewEnter: {
-        flex: 0,
-        borderColor: "gray",
-        borderWidth: 50,
-        backgroundColor: "white",
-        borderWidth: 10,
-        marginTop: 10,
-        justifyContent: "flex-end",
-        alignItems: "center"
-    },
-    signupButton: {
-        color: "#ffffff",
-        fontSize: 16,
-        fontWeight: '500'
-    },
-    containerlogo: {
-        flexGrow: 2,
-        justifyContent: "flex-end",
-        alignItems: "center"
-    },
-    logoText: {
-        marginVertical: 15,
-        fontSize: 18,
-        color: "rgba(255,255,255,0.6)",
-    },
-    containerform: {
-        flexGrow: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
     inputBox: {
-        width: 300,
-        backgroundColor: "rgba(255,255,255,0.3)",
-        borderRadius: 25,
+        width: 350,
+        height: 39,
+        backgroundColor: "rgba(255,255,255,0.4)",
+        borderRadius: 15,
         paddingHorizontal: 16,
         fontSize: 16,
         color: '#ffffff',
         marginVertical: 10,
+        justifyContent:"flex-start",
+    },
+    inputBox2: {
+        width: 350,
+        height: 159,
+        backgroundColor: "rgba(255,255,255,0.4)",
+        borderRadius: 15,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: '#ffffff',
+        marginVertical: 10,
+        justifyContent:"flex-start",
     },
     button: {
-        width: 300,
-        backgroundColor: "#1c313a",
+        backgroundColor: "#293438",
         borderRadius: 25,
-        marginVertical: 10,
-        paddingVertical: 12,
-        alignItems: "center"
+        marginVertical: 30,
+        paddingVertical: 7,
+        paddingHorizontal: 20,
+        alignItems: "center",
+        justifyContent: "center",
     },
-    buttonText: {
-        fontSize: 16,
-        fontWeight: '500',
-        color: '#ffffff'
+    Text: {
+        width: 300,
+        height: 55,
+        fontSize: 25,
+        color: "#ffffff",
+    },
+    space:{
+      width: 17,
+      height: 17,
+    },
+    header: {
+        height: 60, 
+        justifyContent: 'space-between', 
+        alignItems: "center", 
+        backgroundColor: "#363636", 
+        flexDirection: 'row',
+    },
+    textHeader: {
+        color:'white', 
+        fontSize: 18,
+        fontWeight: '600', 
+    },
+    imageDevocional: {
+        width: 400,
+        height: 190, 
+        justifyContent:"center", 
+        alignItems:"center", 
     }
 });
+
