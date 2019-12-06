d2ce9504ed6db61196dca710ff25e1190ec075cc
Fernanda Stella <fstella@uolinc.com>
2019-12-06 11:49:51 -0300

    Desenvolvimento userPage

--- src/pages/userPage.js
+++ src/pages/userPage.js
@@ -3,121 +3,193 @@ import {
     StyleSheet,
     Text,
     View,
-    StatusBar,
-    Image,
-    TextInput,
     TouchableOpacity,
-    TouchableHighlight,
-    ImageBackground,
-} from "react-native"
+    Image,
+    ScrollView,
+    FlatList,
+    Alert,
+    ActivityIndicator,
+} from "react-native";
+import { Actions } from "react-native-router-flux";
+import { Card, CheckBox } from 'react-native-elements';
+import Axios from "axios";
+import API from "../config/AppConstants"
+import colors from "../styles/colors";
 
-
-export default class ContactPage extends Component {
-
+export default class UserPage extends Component {
     constructor() {
         super();
         this.state = {
-            email: "",
-            password: ""
+            name: [],
+            email: [],
+            checked: [],
+            loading: false
         }
     }
     render() {
-        console.log(this.state)
         return (
-            <ImageBackground source={require("../images/tela.jpg")} style={styles.container}>
-                <StatusBar
-                    backgroundColor={"transparent"}
-                    translucent
-                    barStyle={"light-content"}
-                />
-                <View style={styles.containerlogo}>
-                    <Image style={{ width: 150, height: 121 }}
-                        source={require('../images/logo.png')} />
-                    <Text style={styles.logoText}>Estamos na tela user</Text>
+            <ScrollView style={{ backgroundColor: '#3E3E3E' }}>
+                <View style={styles.container}>
+                    <View style={styles.space}></View>
+
+                    <View style={styles.header}>
+                        <TouchableOpacity onPress={() => Actions.pop()}>
+                            <Image style={styles.containerlogo}
+                                source={require('../images/voltar.png')} />
+                        </TouchableOpacity>
+                        <Text style={styles.titleHeader}> Controle de usuários</Text>
+                    </View>
+
+                    <View style={styles.space}></View>
+
+                        <Card
+                            containerStyle={{ backgroundColor: 'grey', borderColor: 'grey' }}
+                            wrapperStyle={{ backgroundColor: "grey" }}>
+                            <Text numberOfLines={1} style={{ marginBottom: 15, color: 'white', fontSize: 16 }}>
+                                {item.name} 
+                            </Text>
+                            <Text numberOfLines={1} style={{ marginBottom: 15, color: 'white', fontSize: 16 }}>
+                                {item.name}   {item.email}
+                            </Text>
+                        </Card>
+
+                        <CheckBox
+                            center
+                            title='Adicionar'
+                            checkedIcon='dot-circle-o'
+                            uncheckedIcon='circle-o'
+                            checked={this.state.checked}
+                        />
+                        <CheckBox
+                            checkedIcon={<Image source={require('../images/checked.png')} />} //imageeees
+                            uncheckedIcon={<Image source={require('../images/unchecked.png')} />} //imagesss
+                            checked={this.state.checked}
+                            onPress={() => this.setState({ checked: !this.state.checked })}
+                        />
                 </View>
-                <View style={styles.singupTextCont}>
-                    <Text style={styles.singupText}>Don't' have an account yet?</Text>
-                    <Text style={styles.signupButton}>Singup</Text>
-                </View>
-            </ImageBackground>
+            </ScrollView>
         )
     }
-
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#455a64",
+        backgroundColor: "#3E3E3E",
+    },
+    container2: {
+        flex: 1,
+        backgroundColor: "#3E3E3E",
+        alignItems: "center",
+    },
+    Text: {
+        fontSize: 25,
+        color: "#ffffff",
+    },
+    space: {
+        width: 17,
+        height: 17,
+    },
+    baseText: {
+        color: 'white',
+        fontSize: 16,
+        alignItems: 'flex-start',
+        width: 400,
+    },
+    header: {
+        height: 60,
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        backgroundColor: "#363636",
+        flexDirection: 'row',
+    },
+    titleHeader: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    imageDevocional: {
+        width: 400,
+        height: 190,
         justifyContent: "center",
         alignItems: "center",
     },
-    viewLogo: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center"
-    },
-    singupText: {
-        color: "rgba(255,255,255,0.6)",
-        fontSize: 16
-    },
-    singupTextCont: {
-        flexGrow: 1,
+    containerlogo: {
         justifyContent: "center",
-        alignItems: "flex-end",
-        paddingVertical: 16,
-        flexDirection: "row"
+        alignItems: "flex-start",
+        width: 20,
+        height: 11,
     },
-    viewEnter: {
-        flex: 0,
-        borderColor: "gray",
-        borderWidth: 50,
-        backgroundColor: "white",
-        borderWidth: 10,
-        marginTop: 10,
-        justifyContent: "flex-end",
-        alignItems: "center"
-    },
-    signupButton: {
-        color: "#ffffff",
-        fontSize: 16,
-        fontWeight: '500'
+    blocsMenu: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        flexDirection: 'row',
     },
     containerlogo: {
-        flexGrow: 2,
-        justifyContent: "flex-end",
-        alignItems: "center"
-    },
-    logoText: {
-        marginVertical: 15,
-        fontSize: 18,
-        color: "rgba(255,255,255,0.6)",
-    },
-    containerform: {
-        flexGrow: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "flex-start",
+        width: 20,
+        height: 11,
     },
-    inputBox: {
-        width: 300,
-        backgroundColor: "rgba(255,255,255,0.3)",
-        borderRadius: 25,
-        paddingHorizontal: 16,
-        fontSize: 16,
-        color: '#ffffff',
-        marginVertical: 10,
+
+    container: {
+        flex: 1,
+        backgroundColor: '#3E3E3E',
     },
-    button: {
-        width: 300,
-        backgroundColor: "#1c313a",
-        borderRadius: 25,
-        marginVertical: 10,
-        paddingVertical: 12,
-        alignItems: "center"
+    space: {
+        width: 17,
+        height: 17,
     },
-    buttonText: {
-        fontSize: 16,
-        fontWeight: '500',
-        color: '#ffffff'
+    icones: {
+        width: 200,
+        height: 200,
+        alignSelf: "center"
+    },
+    containerlogo: {
+        justifyContent: "center",
+        alignItems: "flex-start",
+        width: 50,
+        height: 41,
+    },
+    header: {
+        height: 60,
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        backgroundColor: "#363636",
+        flexDirection: 'row',
+    },
+    titleHeader: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    viewNotifi: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        flexDirection: 'row',
+        borderColor: 'white',
+    },
+    textNotifi: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: '400',
+    },
+    containerlogo: {
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        width: 20,
+        height: 21,
+    },
+    activityIndicatorWrapper: {
+        backgroundColor: 'transparent',
+        marginTop: "50%",
+        // borderRadius: 50,
+        alignSelf: 'center',
+
+        // justifyContent: 'center',
     }
 });
+
+
+
